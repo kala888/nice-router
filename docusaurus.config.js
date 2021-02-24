b1f8bc3673fe8146abaf0f03db95363c053c8786
kala888 <kala.niubility@gmail.com>
2021-02-24 14:17:38 +0800

    修改配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https:/nice-router.github.com',
+  baseUrl: '/nice-router/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'nice-router', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
